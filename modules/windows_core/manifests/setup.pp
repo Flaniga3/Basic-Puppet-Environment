5fc4c126cc6d4233bdc648f0de77330f9d3430d7
Joseph Flanigan <Flaniga3@gmail.com>
2018-05-29 09:27:44 -0500

    Update setup.pp

--- modules/windows_core/manifests/setup.pp
+++ modules/windows_core/manifests/setup.pp
@@ -1,16 +1,6 @@
 # Installs key software for windows servers
 class windows_core::setup () {
-  # include vagrant
-
-  class { 'vscode':
-    package_ensure                => 'present',
-    vscode_download_absolute_path => 'C:\\Windows\\Temp',
-    create_desktop_icon           => true,
-    create_quick_launch_icon      => true,
-    create_context_menu_files     => true,
-    create_context_menu_folders   => true,
-    add_to_path                   => true,
-  }
+  include chocolatey
   
   file { 'C:\Test-File.txt':
     ensure  => present,
