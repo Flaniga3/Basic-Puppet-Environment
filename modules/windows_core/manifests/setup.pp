a7b24d60a8f63c2347c99d5d8337a621164da55a
Joseph Flanigan <Flaniga3@gmail.com>
2018-05-29 09:19:59 -0500

    Update setup.pp

--- modules/windows_core/manifests/setup.pp
+++ modules/windows_core/manifests/setup.pp
@@ -1,7 +1,6 @@
 # Installs key software for windows servers
 class windows_core::setup () {
   include vagrant
-  include docker
 
   class { 'vscode':
     package_ensure                => 'present',
