62b505b6b9a76869d628f27c5400cc0d95a393dd
Joseph Flanigan <Flaniga3@gmail.com>
2018-05-29 09:12:05 -0500

    Update setup.pp

--- modules/windows_core/manifests/setup.pp
+++ modules/windows_core/manifests/setup.pp
@@ -4,10 +4,16 @@ class windows_core::setup () {
   include docker
 
   class { 'vscode':
-    package_ensure => 'present',
+    package_ensure                => 'present',
+    vscode_download_absolute_path => 'C:\\Windows\\Temp',
+    create_desktop_icon           => true,
+    create_quick_launch_icon      => true,
+    create_context_menu_files     => true,
+    create_context_menu_folders   => true,
+    add_to_path                   => true,
   }
   
-  file {'C:\Test-File.txt':
+  file { 'C:\Test-File.txt':
     ensure  => present,
     content => "Hello World!",
   }
